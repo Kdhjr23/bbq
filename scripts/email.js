2f5bf0b84eaa8620c7413f71bdea5e9dc5a34ffc
Keith Hill <kdhjr23@gmail.com>
2013-11-24 09:35:35 -0500

    qwe

---
+++ scripts/email.js
@@ -0,0 +1,28 @@
+  function onSuccess(data, status)
+        {
+            data = $.trim(data);
+            $("#notification").text(data);
+        }
+  
+        function onError(data, status)
+        {
+            // handle an error
+        }        
+  
+        $(document).ready(function() {
+            $("#submit").click(function(){
+  
+                var formData = $("#callAjaxForm").serialize();
+  
+                $.ajax({
+                    type: "POST",
+                    url: "http://kdemail.comxa.com/entry.php",
+                    cache: false,
+                    data: formData,
+                    success: onSuccess,
+                    error: onError
+                });
+  
+                return false;
+            });
+        });
